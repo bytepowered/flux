25ca7b1354abb2782d00c2dbf648cf10a2c870d8
chenyongjia <yongjia.chen@hotmail.com>
2020-03-08 20:58:46 +0800

    Exports init logger

--- server/run.go
+++ server/run.go
@@ -4,19 +4,22 @@ import (
 	"github.com/bytepowered/flux"
 	"github.com/bytepowered/flux/extension"
 	"github.com/bytepowered/flux/internal"
+	"github.com/bytepowered/flux/logger"
 )
 
-func Run(ver flux.BuildInfo) {
-	//init logger
-	logger, err := internal.InitLogger()
-	if err != nil && logger != nil {
-		logger.Panicf("FluxServer logger init: %v", err)
+func InitDefaultLogger() {
+	l, err := internal.InitLogger()
+	if err != nil && l != nil {
+		l.Panicf("FluxServer logger init: %v", err)
 	} else {
-		extension.SetLogger(logger)
+		extension.SetLogger(l)
 	}
-	if nil == logger {
+	if nil == l {
 		panic("logger is nil")
 	}
+}
+
+func Run(ver flux.BuildInfo) {
 	app := NewFluxServer()
 	if err := app.Init(extension.LoadConfig()); nil != err {
 		logger.Panicf("FluxServer init: %v", err)
