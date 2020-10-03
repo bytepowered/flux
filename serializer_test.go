bf533308c87b551c6e1b0281f637b0f956edfbca
chenyongjia <yongjia.chen@hotmail.com>
2020-10-03 15:20:44 +0800

    test: Add json serializer benchmark test

---
+++ serializer_test.go
@@ -0,0 +1,69 @@
+package flux
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type _serializeKvPair struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+var toUnmarshalJsonData = []byte(`[
+		{"name": "user.name", "value": "yongjia.chen"},
+		{"name": "user.age",    "value": "18"}
+	]`)
+
+var toMarshalJsonData = []_serializeKvPair{
+	{Name: "user.name", Value: "yongjia.chen"},
+	{Name: "user.age", Value: "18"},
+}
+
+func Benchmark_SerializeUnmarshal(b *testing.B) {
+	serializer := NewJsonSerializer()
+	_BenchmarkUnmarshalWith(b, serializer.Unmarshal)
+}
+
+func Benchmark_StdLibUnmarshal(b *testing.B) {
+	_BenchmarkUnmarshalWith(b, json.Unmarshal)
+}
+
+func Benchmark_StdLibMarshal(b *testing.B) {
+	_BenchmarkMarshalWith(b, json.Marshal)
+}
+
+func Benchmark_SerializeMarshal(b *testing.B) {
+	serializer := NewJsonSerializer()
+	_BenchmarkMarshalWith(b, serializer.Marshal)
+}
+
+func _BenchmarkUnmarshalWith(b *testing.B, unmarshaler func(data []byte, v interface{}) error) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		var pairs []_serializeKvPair
+		err := unmarshaler(toUnmarshalJsonData, &pairs)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if 2 != len(pairs) {
+			b.Fatalf("array size not match, was: %d", len(pairs))
+		}
+		if "18" != pairs[1].Value {
+			b.Fatalf("value not match, was: %s", pairs[1].Value)
+		}
+	}
+}
+
+func _BenchmarkMarshalWith(b *testing.B, marshaler func(v interface{}) ([]byte, error)) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		bs, err := marshaler(toMarshalJsonData)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if string(bs) != `[{"name":"user.name","value":"yongjia.chen"},{"name":"user.age","value":"18"}]` {
+			b.Fatalf("data not match, was: %s", string(bs))
+		}
+	}
+}
