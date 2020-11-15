e00afb0f225b1b6cee70c1051e6fc8c5e1c00384
chenyongjia <yongjia.chen@hotmail.com>
2020-11-15 12:13:10 +0800

    test: Add testcase

---
+++ server/argvalue_test.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"github.com/bytepowered/flux"
+	"github.com/bytepowered/flux/support"
+	"github.com/spf13/cast"
+	assert2 "github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDefaultArgumentValueLookupFunc(t *testing.T) {
+	values := map[string]interface{}{
+		"path":   "hahaha",
+		"query":  "query-val",
+		"form":   123,
+		"param":  true,
+		"header": "UA",
+		"attr":   "attr",
+		"attrs":  map[string]string{"key": "value"},
+		"auto":   "auto",
+	}
+	valctx := support.NewValuesContext(values)
+	assert := assert2.New(t)
+	cases := []struct {
+		scope  string
+		key    string
+		expect interface{}
+	}{
+		{scope: flux.ScopePath, key: "path", expect: "hahaha"},
+		{scope: flux.ScopeQuery, key: "query", expect: "query-val"},
+		{scope: flux.ScopeForm, key: "form", expect: 123},
+		{scope: flux.ScopeForm, key: "param", expect: true},
+		{scope: flux.ScopeHeader, key: "header", expect: "UA"},
+		{scope: flux.ScopeAttr, key: "attr", expect: "attr"},
+		{scope: flux.ScopeAuto, key: "auto", expect: "auto"},
+	}
+	for _, c := range cases {
+		mtv, err := DefaultArgumentValueLookupFunc(c.scope, c.key, valctx)
+		assert.NoError(err, "must no error")
+		assert.Equal(cast.ToString(c.expect), mtv.Value)
+	}
+}
