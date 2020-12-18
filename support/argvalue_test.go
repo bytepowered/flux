d82cf0581e6f20a152d46b7a7d0ae1db041c4561
chenyongjia <yongjia.chen@hotmail.com>
2020-12-18 23:56:25 +0800

    ✅: Add more test case for value lookup

--- support/argvalue_test.go
+++ support/argvalue_test.go
@@ -3,18 +3,35 @@ package support
 import (
 	"github.com/bytepowered/flux"
 	assert2 "github.com/stretchr/testify/assert"
+	"net/http"
+	"net/url"
 	"testing"
 )
 
+func TestNilContext(t *testing.T) {
+	assert := assert2.New(t)
+	// Scope & key
+	_, err0 := DefaultArgumentValueLookupFunc("", "", NewEmptyContext())
+	assert.Error(err0, "must error")
+	// Nil context
+	_, err1 := DefaultArgumentValueLookupFunc("a", "b", nil)
+	assert.Error(err1, "must error")
+}
+
 func TestDefaultArgumentValueLookupFunc(t *testing.T) {
 	values := map[string]interface{}{
-		"path":   "hahaha",
-		"query":  "query-val",
-		"form":   123,
-		"param":  true,
-		"header": "UA",
-		"attr":   "attr",
-		"auto":   "auto",
+		"path":          "hahaha",
+		"path-values":   url.Values{},
+		"query":         "query-val",
+		"query-values":  url.Values{},
+		"form":          123,
+		"form-values":   url.Values{},
+		"param":         true,
+		"header":        "UA",
+		"header-values": http.Header{},
+		"attr":          "attr",
+		"auto":          "auto",
+		"value":         "value",
 	}
 	valctx := NewValuesContext(values)
 	cases := []struct {
@@ -29,6 +46,11 @@ func TestDefaultArgumentValueLookupFunc(t *testing.T) {
 		{scope: flux.ScopeHeader, key: "header", expect: flux.WrapTextMTValue("UA")},
 		{scope: flux.ScopeAuto, key: "auto", expect: flux.WrapObjectMTValue("auto")},
 		{scope: flux.ScopeAttr, key: "attr", expect: flux.WrapObjectMTValue("attr")},
+		{scope: flux.ScopeValue, key: "value", expect: flux.WrapObjectMTValue("value")},
+		{scope: flux.ScopePathMap, key: "path-values", expect: flux.WrapStrValuesMapMTValue(url.Values{})},
+		{scope: flux.ScopeQueryMap, key: "query-values", expect: flux.WrapStrValuesMapMTValue(url.Values{})},
+		{scope: flux.ScopeFormMap, key: "form-query", expect: flux.WrapStrValuesMapMTValue(url.Values{})},
+		{scope: flux.ScopeHeaderMap, key: "header-map", expect: flux.WrapStrValuesMapMTValue(url.Values{})},
 	}
 	assert := assert2.New(t)
 	for _, c := range cases {
