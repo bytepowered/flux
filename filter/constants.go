ec6b61d82817ad3b610c00d80c3d36b1bac849cc
baixx <microbytepro@outlook.com>
2021-02-05 10:29:27 +0800

    🎨 Improve: 更新Filter默认参数Key格式为snake_case

--- filter/constants.go
+++ filter/constants.go
@@ -1,8 +1,12 @@
 package filter
 
 const (
-	ConfigKeyCacheExpiration = "cache-expiration"
-	ConfigKeyCacheDisabled   = "cache-disabled"
-	ConfigKeyCacheSize       = "cache-size"
-	ConfigKeyDisabled        = "disabled"
+	ConfigKeyCacheExpiration   = "cache_expiration"
+	ConfigKeyCacheDisabled     = "cache_disabled"
+	ConfigKeyCacheSize         = "cache_size"
+	ConfigKeyDisabled          = "disabled"
+	ConfigKeyProviderInterface = "provider_interface"
+	ConfigKeyProviderAddress   = "provider_address"
+	ConfigKeyProviderMethod    = "provider_method"
+	ConfigKeyProviderPreload   = "provider_preload"
 )
