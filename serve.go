a6fee20c83f6ae3fd461189b2c865ff56cd3965e
topbaixx <microbytepro@outlook.com>
2021-06-01 19:06:02 +0800

    refactor: Move error code/message

--- serve.go
+++ serve.go
@@ -5,51 +5,6 @@ import (
 	"net/http"
 )
 
-const (
-	ErrorCodeGatewayInternal    = "GATEWAY:INTERNAL"
-	ErrorCodeGatewayTransporter = "GATEWAY:TRANSPORTER"
-	ErrorCodeGatewayEndpoint    = "GATEWAY:ENDPOINT"
-)
-
-const (
-	ErrorCodeRequestInvalid   = "GATEWAY:REQUEST:INVALID"
-	ErrorCodeRequestNotFound  = "GATEWAY:REQUEST:NOT_FOUND"
-	ErrorCodeRequestCircuited = "GATEWAY:REQUEST:CIRCUITED"
-	ErrorCodeRequestCanceled  = "GATEWAY:REQUEST:CANCELED"
-)
-
-const (
-	ErrorCodePermissionDenied = "GATEWAY:PERMISSION:ACCESS_DENIED"
-)
-
-const (
-	ErrorCodeJwtMalformed = "GATEWAY:AUTHORIZATION:JWT:MALFORMED"
-	ErrorCodeJwtExpired   = "GATEWAY:AUTHORIZATION:JWT:EXPIRED"
-	ErrorCodeJwtNotFound  = "GATEWAY:AUTHORIZATION:JWT:NOTFOUND"
-)
-
-const (
-	ErrorMessageProtocolUnknown = "GATEWAY:PROTOCOL:UNKNOWN"
-
-	// Transport errors
-	ErrorMessageTransportDubboInvokeFailed        = "TRANSPORT:DU:INVOKE/error"
-	ErrorMessageTransportDubboAssembleFailed      = "TRANSPORT:DU:ASSEMBLE/error"
-	ErrorMessageTransportDubboClientCanceled      = "TRANSPORT:DU:CANCELED/client"
-	ErrorMessageTransportDubboDecodeInvalidHeader = "TRANSPORT:DU:DECODE:INVALID/headers"
-	ErrorMessageTransportDubboDecodeInvalidStatus = "TRANSPORT:DU:DECODE:INVALID/status"
-	ErrorMessageTransportHttpInvokeFailed         = "TRANSPORT:HT:INVOKE/error"
-	ErrorMessageTransportHttpAssembleFailed       = "TRANSPORT:HT:ASSEMBLE/error"
-	ErrorMessageTransportCodecError               = "TRANSPORT:CODEC/error"
-
-	ErrorMessagePermissionAccessDenied    = "PERMISSION:ACCESS_DENIED"
-	ErrorMessagePermissionServiceNotFound = "PERMISSION:SERVICE:NOT_FOUND"
-	ErrorMessagePermissionVerifyError     = "PERMISSION:VERIFY:ERROR"
-
-	ErrorMessageWebServerRequestNotFound = "SERVER:REQUEST:NOT_FOUND"
-
-	ErrorMessageRequestPrepare = "REQUEST:BODY:PREPARE"
-)
-
 var _ error = new(ServeError)
 
 // ServeError 定义网关处理请求的服务错误；
